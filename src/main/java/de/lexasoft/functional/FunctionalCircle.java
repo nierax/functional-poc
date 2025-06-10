7ea1aedba06c4ffc99ae54fb1c273c5675916af9
nierax <axel.niering@web.de>
2025-06-10 18:00:25 +0200

    Introduced circle calculations

---
+++ src/main/java/de/lexasoft/functional/FunctionalCircle.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package de.lexasoft.functional;
+
+import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ */
+public class FunctionalCircle {
+
+  private static Logger logger = Logger.getLogger(FunctionalCircle.class.getName());
+
+  public final static Function<Double, Double> ference = diameter -> Math.PI * diameter;
+  public final static Function<Double, Double> area = diameter -> Math.PI * Math.pow((diameter/2),2);
+  
+  public final static void main(String[] args) {
+    logger.log(Level.INFO,ference.apply(3d).toString());
+    logger.log(Level.INFO,area.apply(3d).toString());
+  }
+
+}
