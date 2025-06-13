b417bb33af82b9fd112b35061b2ea93c7bd16fd1
nierax <axel.niering@web.de>
2025-06-13 23:35:45 +0200

    Test adapted to be parametrized

--- src/test/java/de/lexasoft/objectoriented/CircleTest.java
+++ src/test/java/de/lexasoft/objectoriented/CircleTest.java
@@ -2,31 +2,59 @@ package de.lexasoft.objectoriented;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CircleTest {
 
-  Circle cut;
-
   @BeforeEach
   void setUp() throws Exception {
-    cut = Circle.ofDiameter(3d);
   }
 
-  @Test
-  void testRadius() {
-    assertEquals(1.5d, cut.getRadius());
+  static Stream<Arguments> testRadius() {
+    return Stream.of(//
+        Arguments.of(3.0, 1.5), //
+        Arguments.of(4.0, 2.0), //
+        Arguments.of(0, 0));
   }
 
-  @Test
-  void testFerence() {
-    assertEquals(9.42477796076938, cut.getFerence());
+  @ParameterizedTest
+  @MethodSource
+  void testRadius(double diameter, double expected) {
+    Circle cut = Circle.ofDiameter(diameter);
+    assertEquals(expected, cut.getRadius(), 0.0000001);
   }
 
-  @Test
-  void testArea() {
-    assertEquals(7.0685834705770345, cut.getArea());
+  static Stream<Arguments> testFerence() {
+    return Stream.of(//
+        Arguments.of(3.0, 9.4247779607693797), //
+        Arguments.of(4.0, 12.5663706143591729), //
+        Arguments.of(0, 0));
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void testFerence(double diameter, double expected) {
+    Circle cut = Circle.ofDiameter(diameter);
+    assertEquals(expected, cut.getFerence(), 0.0000001);
+  }
+
+  static Stream<Arguments> testArea() {
+    return Stream.of(//
+        Arguments.of(3.0, 7.0685834705770347), //
+        Arguments.of(4.0, 12.5663706143591729), //
+        Arguments.of(0, 0));
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void testArea(double diameter, double expected) {
+    Circle cut = Circle.ofDiameter(diameter);
+    assertEquals(expected, cut.getArea());
   }
 
 }
