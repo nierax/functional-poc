afa47c470a4648c8666acf1c85b5976e3f39cf53
nierax <axel.niering@web.de>
2025-06-16 18:58:35 +0200

    Vavr introduced

---
+++ src/main/java/de/lexasoft/functional/VavrTry.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package de.lexasoft.functional;
+
+import io.vavr.control.Try;
+
+/**
+ * 
+ */
+public class VavrTry {
+  // = Success(result) or Failure(exception)
+  Try<Integer> divide(Integer dividend, Integer divisor) {
+    return Try.of(() -> dividend / divisor);
+  }
+
+  public final static void main(String[] args) {
+    VavrTry vavrTry = new VavrTry();
+    Try<Integer> result = vavrTry.divide(2, 2);
+    result.get();
+  }
+
+}
